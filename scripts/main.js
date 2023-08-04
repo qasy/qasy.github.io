d5c56f286a72d8b4507a6cad335c60c3c96969b9
Aleksey <aleksey.ivanov@apstecsystems.com>
2023-08-04 17:24:31 +0300

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,30 @@
-var myImage = document.querySelector('img');
+var myImage = document.querySelector("img");
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === './images/index-coffee-1.jpg') {
-      myImage.setAttribute('src','./images/index-coffee-2.jpeg');
-    } else {
-      myImage.setAttribute('src','./images/index-coffee-1.jpg');
-    }
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "./images/index-coffee-1.jpg") {
+    myImage.setAttribute("src", "./images/index-coffee-2.jpeg");
+  } else {
+    myImage.setAttribute("src", "./images/index-coffee-1.jpg");
+  }
+};
+
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+
+function setUserName() {
+  var myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Mozilla is cool, " + myName;
 }
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName()
-{
-    var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is cool, " + storedName;
 }
 
-if(!localStorage.getItem('name'))
-{
-    setUserName();
-}
-else
-{
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function(){ setUserName(); }
-
+myButton.onclick = function () {
+  setUserName();
+};
